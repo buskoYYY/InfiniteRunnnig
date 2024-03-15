65b48c33b6c4c61acb27a915e75f24b19d95c0d8
buskoYYY <busko.y.y.y@gmail.com>
2024-03-15 12:28:27 +0300

    Instanciate  StreetLights

--- InfiniteRunner/Assets/Scripts/WorldGenerator.cs
+++ InfiniteRunner/Assets/Scripts/WorldGenerator.cs
@@ -8,12 +8,15 @@ public class WorldGenerator : MonoBehaviour
     [Header("Elements")]
     [SerializeField] Transform startPoint;
     [SerializeField] Transform endPoint;
-    [SerializeField] Transform[] buildingSpawnPoint;
+    [SerializeField] Transform[] buildingSpawnPoints;
+    [SerializeField] Transform[] streetLightSpawnPoints;
     [SerializeField] GameObject[] roadBlocks;
     [SerializeField] GameObject[] buildings;
+    [SerializeField] GameObject streetLight;
 
     [Header("Settings")]
     [SerializeField] float envMoveSpeed = 4;
+    [SerializeField] Vector2 buildingSpawnScaleRange = new Vector2(0.6f,0.8f);
     Vector3 moveDirection;
     void Start()
     {
@@ -44,8 +47,40 @@ public class WorldGenerator : MonoBehaviour
             movementComp.SetMoveDir(moveDirection);
             movementComp.SetDestination(endPoint.position);
         }
+
+        SpawnBuilding(newBlock);
+        SpawnStreetLights(newBlock);
+
         return newBlock;
     }
+
+    private void SpawnStreetLights(GameObject parentBlock)
+    {
+        foreach(Transform streetLightSpawnPoint in streetLightSpawnPoints)
+        {
+            Vector3 spawnLoc = parentBlock.transform.position + (streetLightSpawnPoint.position - startPoint.position);
+            Quaternion spawnRot = Quaternion.LookRotation((startPoint.position - streetLightSpawnPoint.position).normalized, Vector3.up);
+            Quaternion SpawnRotOffset = Quaternion.Euler(0, -90, 0);
+            GameObject newStreetLight = Instantiate(streetLight, spawnLoc, spawnRot * SpawnRotOffset, parentBlock.transform);
+        }
+    }
+
+    private void SpawnBuilding(GameObject parentBlock)
+    {
+        foreach (Transform buildingSpawnPoint in buildingSpawnPoints)
+        {
+            Vector3 buildingSpawnLoc = parentBlock.transform.position + (buildingSpawnPoint.position - startPoint.position);
+            int rotationOffsetBy90 = Random.Range(0, 3);
+            Quaternion spawnRotation = Quaternion.Euler(0, rotationOffsetBy90 * 90, 0);
+            Vector3 buildingSpawnSize = Vector3.one * Random.Range(buildingSpawnScaleRange.x, buildingSpawnScaleRange.y);
+
+            int buildingPick = Random.Range(0, buildings.Length);
+
+            GameObject newBuilding = Instantiate(buildings[buildingPick], buildingSpawnLoc, spawnRotation, parentBlock.transform);
+            newBuilding.transform.localScale = buildingSpawnSize;
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject != null)
